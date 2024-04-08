7972c76fbe670468baafa9ffb38b7d66d048aa1f
Eemil74 <Eemil.k.k.2007@gmail.com>
2024-04-08 13:18:37 +0300

    .

--- Tehtävä 5/painoindeksi.php
+++ Tehtävä 5/painoindeksi.php
@@ -10,7 +10,7 @@
     <input type="text" name="num1" placeholder="Syötä paino">
     <input type="text" name="num2" placeholder="Syötä pituus">
     <input type="submit" name="submit" value="Laske">
-    <input type="reset">
+    <input type="button" value="Tyhjennä" onclick="resetFields()">
     </form>
 <?php 
 if(isset($_POST['submit'])){
@@ -20,5 +20,12 @@ $vastaus = $num1 * 1.3 / pow(($num2/100),2.5);
 $vastaus = round($vastaus,2);
 echo "<p>Tulos: $vastaus</p>";}
 ?>
+<script>
+function resetFields() {
+    document.getElementsByName('num1')[0].value = '';
+    document.getElementsByName('num2')[0].value = '';
+    document.querySelector('p').innerHTML = ''; // Tyhjentää vastauksen
+}
+</script>
 </body>
 </html>
