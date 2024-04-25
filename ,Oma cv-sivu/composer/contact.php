129094e702654dca5a104ad94499c2ab9383f546
Eemil74 <Eemil.k.k.2007@gmail.com>
2024-04-25 11:18:33 +0300

    .

--- ,Oma cv-sivu/composer/contact.php
+++ ,Oma cv-sivu/composer/contact.php
@@ -1,37 +1,37 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "eemil";
 
-//Load Composer's autoloader
-require 'vendor/autoload.php';
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.freesmtpservers.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'Eemil.kuittinen07@outlook.com';                     //SMTP username
-    $mail->Password   = 'Pelihullu07';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 25;                                    //TCP port to connect to; use 25 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    $mail->setFrom($_POST['email'],$_POST['name']);
-    $mail->addAddress('Eemil.kuittinen07@outlook.com','Minä');
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject =$_POST['subject'] ;
-    $mail->Body    = $_POST['message'];
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+// check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $name = mysqli_real_escape_string($conn, $name);
+    $email = mysqli_real_escape_string($conn, $email);
+    $subject = mysqli_real_escape_string($conn, $subject);
+    $message = mysqli_real_escape_string($conn, $message);
+
+    $sql = "INSERT INTO messages (name1, email1, subject1, message1) VALUES ('$name', '$email', '$subject', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Close
+$conn->close();
+header("location:../contact.html");
 ?>
